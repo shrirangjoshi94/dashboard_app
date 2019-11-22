eaf68378d644bd51be06596958d81677f284b059
your <vutran6853@gmail.com>
2019-11-21 21:42:35 -0600

    add tailwind UI framework

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,17 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  extends: ['plugin:vue/essential'],
   rules: {
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "semi": ["error", "never"],
-    "quotes": ["off"],
-    "no-console": ["off"],
-    "eqeqeq": ["error", "always"]
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    semi: ['error', 'never'],
+    quotes: ['off'],
+    'no-console': ['off'],
+    eqeqeq: ['error', 'always'],
+    'max-len': ['off'],
+    'space-before-keywords': ['off']
   },
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   }
-};
+}
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './index.css'
+import '@/assets/css/tailwind.css'
 
 Vue.config.productionTip = false
 
