b362056515f7077bb198eb911e37b747cbac72b2
your <vutran6853@gmail.com>
2019-11-22 18:52:28 -0600

    disable eslint on this project

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,7 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['plugin:vue/essential'],
   rules: {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    semi: ['error', 'never'],
-    quotes: ['off'],
-    'no-console': ['off'],
-    eqeqeq: ['error', 'always'],
-    'max-len': ['off'],
-    'space-before-keywords': ['off']
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
   }
 }
