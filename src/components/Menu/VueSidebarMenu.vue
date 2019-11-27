71e97edc4746ddaef3f4f724c4cc97a11b32d15f
Shrirang <shrirangjoshi94@gmail.com>
2019-11-28 02:24:42 +0530

    Working on the left menu of the dashboard.

---
+++ src/components/Menu/VueSidebarMenu.vue
@@ -0,0 +1,22 @@
+<template>
+    <sidebar-menu :menu="menu"/>
+</template>
+
+<script>
+    export default {
+        data() {
+            menu: [
+                {
+                    href: '/',
+                    title: 'Dashboard',
+                    icon: 'fa fa-user'
+                },
+                {
+                    href: '#',
+                    title: 'Charts',
+                    icon: 'fa fa-chart-area'
+                },
+            ]
+        }
+    }
+</script>
